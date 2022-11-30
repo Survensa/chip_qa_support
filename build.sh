d51f73f6f977e415e9a615851a9f6c7bb38b23f0
Suraj Seenivasan <95559513+Survensa@users.noreply.github.com>
2022-11-30 23:27:58 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,25 @@
 #! /usr/bin/env bash
+
+echo "Hello..!" $USER
+sleep 2
+echo
+read -p "Confirm wheather you have updated build_config.json inputs (Y/N)?:" -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo
+    sleep 5
+    echo "Please update your inputs in build_config.json "
+    echo
+    sleep 2
+    echo "Bye...!"
+    exit
+else
+    echo
+    sleep 5
+    echo "Your build Starts here..."
+    cd scripts/
+    python chip_build.py
+fi
+echo
+echo " Check apps folder for successfull buil "
