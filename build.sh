f948ad185678c3226b08ee407a15d804e76624bf
Suraj Seenivasan <95559513+Survensa@users.noreply.github.com>
2022-12-19 20:26:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,24 +3,24 @@
 echo "Hello..!" $USER
 sleep 2
 echo
-read -p "Confirm wheather you have updated build_config.json inputs (Y/N)?" -n 1 -r
+read -p "Confirm wheather you have updated build_config.json inputs (Y/N)?" ANSWER1
 echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
+if [ "$ANSWER1" == "Y" ] || [ "$ANSWER1" == "y" ]
 then
     echo
-    sleep 5
-    echo "Please update your inputs in build_config.json "
-    echo
     sleep 2
-    echo "Bye...!"
-    exit
-else
-    echo
-    sleep 5
     echo "Your build Starts here..."
     cd ~/chip_command_run/scripts/
     python3 chip_build.py
+    echo
+    echo "Check apps folder for successfull build"
+    exit
+elif [ "$ANSWER1" == "N" ] || [ "$ANSWER1" == "n" ]
+    echo
+    echo "Execution Aborted . . . Please update your inputs in build_config.json "
+    exit
+else
+    echo
+	echo "INVALID INPUT . . . Retry again"
+	exit
 fi
-echo
-echo "Check apps folder for successfull build"
-exit
