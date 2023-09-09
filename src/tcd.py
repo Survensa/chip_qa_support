75342fc1f87a354b1845ed764dc69549e2060134
Suraj Seenivasan <suraj@graniteriverlabs.in>
2023-09-09 17:17:12 +0530

    Create tcd.py

---
+++ src/tcd.py
@@ -0,0 +1,117 @@
+from bs4 import BeautifulSoup
+import openpyxl
+from openpyxl.styles import Font, Alignment
+import re
+
+print("Ready")
+
+# Create an Excel workbook and define the filename
+workbook = openpyxl.Workbook()
+filename = 'Docs/output.xlsx'
+
+app_html = 'Docs/Test_Plan_HTML/allclusters.html'
+main_html = 'Docs/Test_Plan_HTML/index.html'
+
+# Create an Excel sheet
+sheet1 = workbook.active
+sheet1.title = "All_TC_Details"
+
+# Define column headers
+headers = ['S.No', 'Cluster Name', 'Test Case Name', 'Test Case ID', 'Test Plan']
+
+# Add headers to the first row and set the font to bold for the headings
+header_font = Font(name='Times New Roman', bold=True)
+for col_num, header in enumerate(headers, 1):
+    cell = sheet1.cell(row=1, column=col_num, value=header)
+    cell.font = header_font
+
+# Set header row alignment to center
+for cell in sheet1[1]:
+    cell.alignment = Alignment(horizontal='center', vertical='center')
+
+# Define a function to extract test case details
+def extract_tc_details(h1_tags, a):
+    row_number = 1  # Initialize row_number outside the loop
+    for i, h1_tag in enumerate(h1_tags):
+        h1 = h1_tag.text
+        cluster_name = h1.replace('Cluster Test Plan', '')
+
+        first_h1 = h1_tag
+        if i == (len(h1_tags) - 1):
+            second_h1 = False
+        else:
+            second_h1 = h1_tags[i + 1]
+
+        h5_tags = first_h1.find_all_next('h5', {'id': lambda x: x and x.startswith('_test_procedure')})
+        result = []
+
+        if second_h1:
+            for h5_tag in h5_tags:
+                if h5_tag.find_previous('h1') == first_h1:
+                    if h5_tag.find_next('h1') == second_h1:
+                        result.append(h5_tag)
+        else:
+            h5_tags = first_h1.find_all_next('h6', {'id': lambda x: x and x.startswith('_test_procedure')})
+            for h5_tag in h5_tags:
+                if h5_tag.find_previous('h1') == first_h1:
+                    if h5_tag.find_next('h1') == second_h1:
+                        result.append(h5_tag)
+
+        if result:
+            for j in range(len(result)):
+                h4_tag = result[j].find_previous('h4')
+                head_text_match = re.search(r'\[(.*?)\]\s*(.*)', h4_tag.text)
+                if head_text_match:
+                    head_text = '[' + head_text_match.group(1) + '] ' + head_text_match.group(2)
+                else:
+                    head_text = ''
+
+                # Extract the "Test case name" using regular expressions
+                testcase_match = re.search(r'\[(.*?)\]', head_text)
+                if testcase_match:
+                    testcase_name = testcase_match.group(1)  # Extract the first group (inside parentheses)
+                else:
+                    testcase_name = ''
+
+                if a == 0:
+                    test_plan = "Core Test Case"
+                else:
+                    test_plan = "App Test Case"
+
+                # Modify row_values list to include "Test case name"
+                row_values = [row_number, cluster_name, head_text, testcase_name, test_plan]
+                sheet1.append(row_values)
+
+                # Increment row_number for each new row
+                row_number += 1
+
+# Parse 'app' HTML
+with open(app_html, encoding='utf-8') as f1:
+    soup1 = BeautifulSoup(f1, 'html.parser')
+    h1_tags1 = soup1.find_all('h1', {'id': True})
+    extract_tc_details(h1_tags1, 1)
+
+# Parse 'main' HTML
+with open(main_html, encoding='utf-8') as f2:
+    soup2 = BeautifulSoup(f2, 'html.parser')
+    h1_tags2 = soup2.find_all('h1', {'id': True})
+    extract_tc_details(h1_tags2, 0)
+
+# Set the font for the entire sheet to Times New Roman
+for row in sheet1.iter_rows(min_row=2, max_row=sheet1.max_row, min_col=1, max_col=sheet1.max_column):
+    for cell in row:
+        cell.font = Font(name='Times New Roman')
+        cell.alignment = Alignment(vertical='center')  # Center-align vertically
+
+# Set alignment to center for columns A and E
+for column_letter in ['A', 'E']:
+    for cell in sheet1[column_letter]:
+        cell.alignment = Alignment(horizontal='center', vertical='center')
+
+# Set column widths
+column_widths = {'A': 5, 'B': 30, 'C': 100, 'D': 25, 'E': 15}
+for column, width in column_widths.items():
+    sheet1.column_dimensions[column].width = width
+
+# Save the workbook
+workbook.save(filename)
