5ec6126e395f847a6b9cca9137aeaf45ee8e2ad9
Suraj Seenivasan <suraj@graniteriverlabs.in>
2023-09-21 16:41:19 +0530

    Update xlsxtohtml.py

--- src/xlsxtohtml.py
+++ src/xlsxtohtml.py
@@ -9,16 +9,16 @@ def xlsx_to_html(input_file, output_file):
 
     # Open the output file in write mode
     with open(output_file, 'w') as html_file:
-        # Write the table opening tag
-        html_file.write("<table>\n")
+        # Write the table opening tag with CSS styling for borders and header background color
+        html_file.write('<table style="border-collapse: collapse; width: 100%;">\n')
 
-        # Write the table header
-        headers = ["<th>" + (cell.value if cell.value is not None else "") + "</th>" for cell in next(sheet.iter_rows())]
+        # Write the table header with a light gray background color
+        headers = ["<th style='border: 1px solid black; padding: 8px; background-color: #f2f2f2;'>" + (cell.value if cell.value is not None else "") + "</th>" for cell in next(sheet.iter_rows())]
         html_file.write("<tr>" + "".join(headers) + "</tr>\n")
 
         # Write the table rows
         for row in sheet.iter_rows(min_row=2):
-            row_data = [("<td>" + str(cell.value) + "</td>") if cell.value is not None else "<td></td>" for cell in row]
+            row_data = [("<td style='border: 1px solid black; padding: 8px;'>" + str(cell.value) + "</td>") if cell.value is not None else "<td></td>" for cell in row]
             html_file.write("<tr>" + "".join(row_data) + "</tr>\n")
 
         # Write the table closing tag
