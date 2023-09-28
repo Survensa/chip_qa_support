7de0a7af1874c5fee48797bf3a77ad1f726f4c8e
Suraj Seenivasan <suraj@graniteriverlabs.in>
2023-09-28 11:57:20 +0530

    Update tcd.py

--- src/tcd.py
+++ src/tcd.py
@@ -6,43 +6,41 @@ import re
 print("Process Starts")
 
 # Create an Excel workbook and define the filename
-workbook = openpyxl.Workbook()
 filename = 'Docs/TC_Summary.xlsx'
 
 app_html = 'Docs/Test_Plan_HTML/allclusters.html'
 main_html = 'Docs/Test_Plan_HTML/index.html'
 
-# Create an Excel sheet
-sheet1 = workbook.active
-sheet1.title = "All_TC_Details"
+# Load the existing workbook
+workbook = openpyxl.load_workbook(filename)
+sheet1 = workbook['All_TC_Details']
 
-# Define column headers
-headers = ['S.No', 'Cluster Name', 'Test Case Name', 'Test Case ID', 'Test Plan']
+# Create a new sheet for changes
+changes_sheet = workbook.create_sheet(title="Changes")
 
-# Add headers to the first row and set the font to bold for the headings
-header_font = Font(name='Times New Roman', bold=True)
-for col_num, header in enumerate(headers, 1):
-    cell = sheet1.cell(row=1, column=col_num, value=header)
-    cell.font = header_font
-
-# Set header row alignment to center
-for cell in sheet1[1]:
+# Define column headers for the changes sheet
+changes_headers = ['S.No', 'Cluster Name', 'Test Case Name', 'Test Case ID', 'Test Plan', 'Change Type']
+for col_num, header in enumerate(changes_headers, 1):
+    cell = changes_sheet.cell(row=1, column=col_num, value=header)
+    cell.font = Font(name='Times New Roman', bold=True)
     cell.alignment = Alignment(horizontal='center', vertical='center')
 
-# Define a function to extract test case details
-def extract_tc_details(h1_tags, a, row_number):
-    for i, h1_tag in enumerate(h1_tags):
+# Set header row alignment to center for the changes sheet
+for cell in changes_sheet[1]:
+    cell.alignment = Alignment(horizontal='center', vertical='center')
+
+# Function to find changes and append them to the changes sheet
+def find_and_append_changes(html_tags, a):
+    row_number = sheet1.max_row + 1
+    for i, h1_tag in enumerate(html_tags):
         h1 = h1_tag.text
         cluster_name = h1.replace('Cluster Test Plan', '')
 
-        print("-" * 40)
-        print(f"Fetching details for cluster: {cluster_name}")
-
         first_h1 = h1_tag
-        if i == (len(h1_tags) - 1):
+        if i == (len(html_tags) - 1):
             second_h1 = False
         else:
-            second_h1 = h1_tags[i + 1]
+            second_h1 = html_tags[i + 1]
 
         h5_tags = first_h1.find_all_next('h5', {'id': lambda x: x and x.startswith('_test_procedure')})
         result = []
@@ -68,20 +66,25 @@ def extract_tc_details(h1_tags, a, row_number):
                 # Extract the "Test case name" using regular expressions
                 testcase_match = re.search(r'\[(.*?)\]', head_text)
                 if testcase_match:
-                    testcase_name = testcase_match.group(1)  # Extract the first group (inside parentheses)
+                    testcase_name = testcase_match.group(1)
                 else:
                     testcase_name = ''
 
-                if a == 0:
-                    test_plan = "Core Test Case"
+                test_plan = "Core Test Case" if a == 0 else "App Test Case"
+
+                # Check if the row already exists in the All_TC_Details sheet
+                existing_row = sheet1.find(testcase_name, column=3)
+                if existing_row:
+                    # Row exists, compare values
+                    existing_values = [cell.value for cell in sheet1[existing_row.row]]
+                    new_values = [row_number, cluster_name, head_text, testcase_name, test_plan]
+
+                    if existing_values != new_values:
+                        # Values are different, append to the changes sheet as modified
+                        changes_sheet.append(new_values + ["Modified"])
                 else:
-                    test_plan = "App Test Case"
-
-                # Modify row_values list to include "Test case name"
-                row_values = [row_number, cluster_name, head_text, testcase_name, test_plan]
-                sheet1.append(row_values)
-
-                print(f"Fetching details for Test Case: {testcase_name}")
+                    # Row doesn't exist, append to the changes sheet as added
+                    changes_sheet.append([row_number, cluster_name, head_text, testcase_name, test_plan, "Added"])
 
                 # Increment row_number for each new row
                 row_number += 1
@@ -92,10 +95,7 @@ print("Parsing 'app' HTML...")
 with open(app_html, encoding='utf-8') as f1:
     soup1 = BeautifulSoup(f1, 'html.parser')
     h1_tags1 = soup1.find_all('h1', {'id': True})
-    extract_tc_details(h1_tags1, 1, 1)  # Pass initial row_number as 1
-
-# Calculate the next row_number after parsing the first HTML
-row_number = sheet1.max_row + 1
+    find_and_append_changes(h1_tags1, 1)
 
 # Parse 'main' HTML
 print("^" * 40)
@@ -103,26 +103,33 @@ print("Parsing 'main' HTML...")
 with open(main_html, encoding='utf-8') as f2:
     soup2 = BeautifulSoup(f2, 'html.parser')
     h1_tags2 = soup2.find_all('h1', {'id': True})
-    extract_tc_details(h1_tags2, 0, row_number)  # Pass the updated row_number
+    find_and_append_changes(h1_tags2, 0)
+
+# Identify removed items
+for row in sheet1.iter_rows(min_row=2, max_row=sheet1.max_row, min_col=1, max_col=sheet1.max_column):
+    testcase_name = row[3].value
+    if not any(testcase_name in h1_tag.text for h1_tag in h1_tags1 + h1_tags2):
+        # Test case not found in HTML data, append to the changes sheet as removed
+        changes_sheet.append([row[0].value, row[1].value, row[2].value, row[3].value, row[4].value, "Removed"])
 
 # Set the font for the entire sheet to Times New Roman
-for row in sheet1.iter_rows(min_row=2, max_row=sheet1.max_row, min_col=1, max_col=sheet1.max_column):
+for row in changes_sheet.iter_rows(min_row=2, max_row=changes_sheet.max_row, min_col=1, max_col=changes_sheet.max_column):
     for cell in row:
         cell.font = Font(name='Times New Roman')
         cell.alignment = Alignment(vertical='center')  # Center-align vertically
 
-# Set alignment to center for columns A and E
-for column_letter in ['A', 'E']:
-    for cell in sheet1[column_letter]:
+# Set alignment to center for columns A to F in the changes sheet
+for column_letter in ['A', 'B', 'C', 'D', 'E', 'F']:
+    for cell in changes_sheet[column_letter]:
         cell.alignment = Alignment(horizontal='center', vertical='center')
 
-# Set column widths
-column_widths = {'A': 5, 'B': 30, 'C': 100, 'D': 25, 'E': 15}
-for column, width in column_widths.items():
-    sheet1.column_dimensions[column].width = width
+# Set column widths for the changes sheet
+changes_column_widths = {'A': 5, 'B': 30, 'C': 100, 'D': 25, 'E': 15, 'F': 15}
+for column, width in changes_column_widths.items():
+    changes_sheet.column_dimensions[column].width = width
 
-# Save the workbook
-print("Saving Excel workbook...")
+# Save the updated workbook
+print("Saving Excel workbook with changes...")
 workbook.save(filename)
 
-print("Process completed. Excel file saved as 'TC_Summary.xlsx'.")
+print("Process completed. Excel file updated with changes.")
