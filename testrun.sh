1f369ba83cced351ea0897f829fb801118a44a71
Suraj Seenivasan <suraj@graniteriverlabs.in>
2023-10-25 20:53:01 +0530

    Update testrun.sh

--- testrun.sh
+++ testrun.sh
@@ -1,12 +1,17 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 echo "Hello . . . !" $USER
 echo
 read -p "Confirm whether you have updated the Execution Section in config.yaml (Y/N)?" ANSWER1
 
 if [ "$ANSWER1" == "Y" ] || [ "$ANSWER1" == "y" ]; then
-    cd ~/chip_command_run/scripts/
-    python3 chip_command_run.py "$@"
+    if [[ "$1" == "-c" && "$2" == "CADMIN" ]] || [[ "$1" == "--cluster" && "$2" == "CADMIN" ]]; then
+        cd ~/chip_command_run/scripts/
+        python3 chip_cadmin_command_run.py "${@:3}"
+    else
+        cd ~/chip_command_run/scripts/
+        python3 chip_command_run.py "$@"
+    fi
 elif [ "$ANSWER1" == "N" ] || [ "$ANSWER1" == "n" ]; then
     echo
     echo "Execution Aborted . . . Please update the Execution Section in config.yaml"
