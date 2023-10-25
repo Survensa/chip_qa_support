49868de899c07767a1a550748371a08d3a33e4f9
Suraj Seenivasan <suraj@graniteriverlabs.in>
2023-10-25 19:41:05 +0530

    Update testrun.sh

--- testrun.sh
+++ testrun.sh
@@ -5,14 +5,8 @@ echo
 read -p "Confirm whether you have updated the Execution Section in config.yaml (Y/N)?" ANSWER1
 
 if [ "$ANSWER1" == "Y" ] || [ "$ANSWER1" == "y" ]; then
-    if [ "$1" == "-c" ]; then
-        shift
-        cd ~/chip_command_run/scripts/
-        python3 chip_command_run.py -c "$@"
-    else
-        cd ~/chip_command_run/scripts/
-        python3 chip_command_run.py
-    fi
+    cd ~/chip_command_run/scripts/
+    python3 chip_command_run.py "$@"
 elif [ "$ANSWER1" == "N" ] || [ "$ANSWER1" == "n" ]; then
     echo
     echo "Execution Aborted . . . Please update the Execution Section in config.yaml"
