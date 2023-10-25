6edfd15b5b00e43db0d7d6792ce1b7e3cfae6a27
Suraj Seenivasan <suraj@graniteriverlabs.in>
2023-10-25 21:25:18 +0530

    Update chip_cadmin_command_run.py

--- scripts/chip_cadmin_command_run.py
+++ scripts/chip_cadmin_command_run.py
@@ -6,38 +6,37 @@ import yaml
 import re
 import argparse
 from dataclasses import dataclass, fields
-from chip_command_run import Cluster
 import threading
 from fabric import Connection
 import time
 from invoke import UnexpectedExit
 import invoke.exceptions
 
-clusters = fields(Cluster)
+@dataclass
+class Cluster:
+    
+    CADMIN: str = "../commands/cadmin.txt"
 
+clusters = fields(Cluster)
 cluster_name = [field.name for field in clusters]
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description='cluster name')
-
 parser.add_argument('-c','--cluster', nargs='+',help='name of the cluster',choices= cluster_name,default= False)
 parser.add_argument('-p','--pairing',help='Auto-pairing function', default= True)
 args = parser.parse_args()
 
 # Load configuration from YAML file
-homedir = os.path.join(os.path.expanduser('~'), "chip_command_run", "config.yaml")
-with open(homedir, 'r') as file:
+config_path = os.path.join(os.path.expanduser('~'), "chip_command_run", "config.yaml")
+with open(config_path, 'r') as file:
     yaml_info = yaml.safe_load(file)
     build = yaml_info["chip_tool_directory"]
-
-
 
 # Define regular expressions
 pattern1 = re.compile(r'(CHIP:DMG|CHIP:TOO)(.*)')
 pattern2 = re.compile(r'^\./chip-tool')
 pattern3 = re.compile(r'avahi-browse')
 testcase = ""
-
 
 # Folder Path
 path = os.path.join(os.getcwd(),"../commands")
@@ -49,12 +48,9 @@ os.chdir(path)
 def factory_reset( data ):
 
         ssh = Connection(host= data["host"], user=data["username"], connect_kwargs={"password": data["password"]})
-
-
         # Executing the  'ps aux | grep process_name' command to find the PID value to kill
         command = f"ps aux | grep {data['command']}"
         pid_val = ssh.run(command, hide=True)
-
         pid_output = pid_val.stdout
         pid_lines = pid_output.split('\n')
         for line in pid_lines:
@@ -64,23 +60,16 @@ def factory_reset( data ):
                 if conformance == 'Ssl':
                     kill_command = f"kill -9 {pid}"
                     ssh.run(kill_command)
-
-
         ssh.close()
 
 #Function to advertise the dut
 def advertise():
         
         cd = os.getcwd()
-
         data = yaml_info["Dut_data"]
-
-
         ssh = Connection(host= data["host"], user=data["username"], connect_kwargs={"password": data["password"]})
-
         path = data["path"]
         ssh.run('rm -rf /tmp/chip_*')
-
         try:
             log = ssh.run('cd ' + path + ' && ' + data["command"], warn=True, hide=True, pty=False)
         except UnexpectedExit as e:
@@ -88,7 +77,6 @@ def advertise():
                 None
             else:
                 raise
-
         #self.start_logging(log)
         ssh.close()
         logpath = os.path.join(cd,"../logs/execution_logs") 
@@ -97,19 +85,18 @@ def advertise():
             f.write(log.stdout)
         return True
 
-
 # Function to assign the testcase name to the dut-side log
 def testcasename(tc):
     global testcase
     testcase = tc
     return None
-
-
+    
 # Fn to process log files and save them
 def process_log_file(input_file_path, output_directory):
+    
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
-    
+        
     # Construct the output file path using the input file name
     output_file_path = os.path.join(output_directory, os.path.basename(input_file_path))
     avahi = False
@@ -137,14 +124,14 @@ def process_log_file(input_file_path, output_directory):
 
 # Function to scrap the manualcode from the log
 def code():
+    
     with open ("temp.txt", 'r') as f:
         for line in f:
             line = line.strip()
             match = re.search(r'Manual pairing code: \[(\d+)\]', line)
             if match:
                 manualcode = match.group(1)
-                return(str(manualcode))
-            
+                return(str(manualcode))            
     return False
 
     
@@ -193,11 +180,8 @@ def run_command(commands, testcase):
 
             else:
                 cluster_textfile.write('\n' + '\n' + i + '\n' + '\n')
-
         run = subprocess.run(i, shell=True, text=True, stdout= subprocess.PIPE, stderr=subprocess.PIPE)
-
-        log = run.stdout
-                
+        log = run.stdout    
         with open("temp.txt", 'w') as f:
                     f.write(log)
         if "open-commissioning-window" in i:
@@ -206,10 +190,8 @@ def run_command(commands, testcase):
                 None
             else:
                 manualcode = cod
-
         with open(log_file_path, 'a') as cluster_textfile:
             cluster_textfile.write(log)
-
     if pair :
         factory_reset(data)
         time.sleep(5)
@@ -267,25 +249,23 @@ if __name__ == "__main__":
     selected_clusters = args.cluster
 
     build_confirmation = input(f"\nConfirm the Chip-Tool Build Path: {build} (Y/Yes to confirm): ").strip().lower()
-
+    output_directory = os.path.join(os.path.expanduser('~'), "chip_command_run", "logs", "validation_logs")
+    
     if build_confirmation in ['y', 'yes']:
-
         if selected_clusters:
             None
         else:
             selected_clusters = ["CADMIN"]
-
         clusters_confirmation = input(f"\nProceed with selected Clusters {selected_clusters} for execution (Y/Yes to proceed): ").strip().lower()
         print(f"\n****************************************************************")
-
         if clusters_confirmation in ['y', 'yes']:
             if selected_clusters:
                 for cluster_name in selected_clusters:
                     file = vars(Cluster)[cluster_name]
                     file_path = os.path.join(os.path.expanduser('~'), "chip_command_run", "commands", file)
                     read_text_file(file_path)
+                    print(f"\nExecution completed... Logs are ready for validation in {output_directory}")
         else:
-            print("Execution canceled.")
-
+            print(f"\nExecution Canceled With The User Input: {clusters_confirmation}")
     else:
-        print("Execution canceled.")
+        print(f"\nExecution Canceled With User The User Input: {build_confirmation}")
