a20475d7076faecb6ba2ef30151b9561f9dddc43
Suraj Seenivasan <suraj@graniteriverlabs.in>
2023-10-25 19:05:09 +0530

    Update testrun.sh

--- testrun.sh
+++ testrun.sh
@@ -1,20 +1,17 @@
 #! /usr/bin/env bash
 
 echo "Hello . . . !" $USER
-read -p "Confirm wheather you have updated chip_tool_directory and  in config.yaml (Y/N)?" ANSWER1
+read -p "Confirm wheather you have updated the Execution Section in config.yaml (Y/N)?" ANSWER1
 if [ "$ANSWER1" == "Y" ] || [ "$ANSWER1" == "y" ]
 then
 	echo
-	echo "Your Execution Starts here . . ."
 	cd ~/chip_command_run/scripts/
 	python3 chip_command_run.py
-	echo
-	echo "Execution completed . . .  Logs are ready for validation in logs/validation_logs directory"
 	exit
 elif [ "$ANSWER1" == "N" ] || [ "$ANSWER1" == "n" ]
 then
 	echo
-	echo "Execution Aborted . . . Please update chip_tool_directory in config.yaml"
+	echo "Execution Aborted . . . Please update the Execution Section in config.yaml
 	exit
 else
 	echo
