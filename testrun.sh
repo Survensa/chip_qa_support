506492412e66f175e8cf026a7b54ef21e3dd6668
Suraj Seenivasan <suraj@graniteriverlabs.in>
2023-10-25 19:29:10 +0530

    Update testrun.sh

--- testrun.sh
+++ testrun.sh
@@ -5,9 +5,9 @@ echo
 read -p "Confirm whether you have updated the Execution Section in config.yaml (Y/N)?" ANSWER1
 
 if [ "$ANSWER1" == "Y" ] || [ "$ANSWER1" == "y" ]; then
-    if [ "$1" == "-c" ] && [ -n "$2" ]; then
+    if [ -n "$2" ]; then
         cd ~/chip_command_run/scripts/
-        python3 chip_command_run.py -c "$2"
+        python3 chip_command_run.py "$2"
     else
         cd ~/chip_command_run/scripts/
         python3 chip_command_run.py
