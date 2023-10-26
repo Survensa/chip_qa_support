27d9e953b81cdec7f66ac516bf799b951b298d1e
Suraj Seenivasan <suraj@graniteriverlabs.in>
2023-10-26 06:21:18 +0530

    Update testrun.sh

--- testrun.sh
+++ testrun.sh
@@ -6,8 +6,18 @@ read -p "Confirm whether you have updated the Execution Section in config.yaml (
 
 if [ "$ANSWER1" == "Y" ] || [ "$ANSWER1" == "y" ]; then
     if [[ "$1" == "-c" && "$2" == "CADMIN" ]] || [[ "$1" == "--cluster" && "$2" == "CADMIN" ]]; then
-        cd ~/chip_command_run/scripts/
-        python3 chip_cadmin_command_run.py "${@:3}"
+        read -p "Confirm whether you have updated the Cadmin Section in config.yaml (Y/N)?" ANSWER2
+        if [ "$ANSWER2" == "Y" ] || [ "$ANSWER2" == "y" ]; then
+            cd ~/chip_command_run/scripts/
+            python3 chip_cadmin_command_run.py "${@:3}"
+        elif [ "$ANSWER2" == "N" ] || [ "$ANSWER2" == "n" ]; then
+            echo
+            echo "Execution Aborted . . . Please update the Cadmin Section in config.yaml"
+        else
+            echo
+            echo "INVALID INPUT . . . Retry again"
+        fi
+        exit
     else
         cd ~/chip_command_run/scripts/
         python3 chip_command_run.py "$@"
