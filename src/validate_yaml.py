d0eed52f7a0adeeff13d11f54d949043dd669a50
Suraj Seenivasan <suraj@graniteriverlabs.in>
2023-11-05 12:03:56 +0530

    Update validate_yaml.py

--- src/validate_yaml.py
+++ src/validate_yaml.py
@@ -2,16 +2,14 @@ import os
 import yaml
 import sys
 
-def validate_yaml(file_path, count, total_files):
+def validate_yaml(file_path, count, total_files, failed_flag):
     try:
         with open(file_path, 'r') as file:
             yaml.safe_load(file)
         print(f"Validation passed for {file_path} ({count}/{total_files})")
     except Exception as e:
         print(f"Validation failed for {file_path}: {str(e)} ({count}/{total_files})")
-        return False
-
-    return True
+        failed_flag[0] = True
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -24,10 +22,11 @@ if __name__ == '__main__':
     yaml_files = [f for f in os.listdir(os.path.dirname(file_path)) if f.endswith(".yaml")]
 
     total_files = len(yaml_files)
+    failed_flag = [False]
 
     for count, yaml_file in enumerate(yaml_files, start=1):
         file_path = os.path.join(os.path.dirname(file_path), yaml_file)
-        validation_status = validate_yaml(file_path, count, total_files)
+        validate_yaml(file_path, count, total_files, failed_flag)
 
-        if not validation_status:
-            sys.exit(1)
+    if failed_flag[0]:
+        sys.exit(1)
