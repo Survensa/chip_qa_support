710a572e4849ef385129a7264b5ee6a1df515652
Survensa <suraj@graniteriverlabs.in>
2023-11-09 13:56:16 +0000

    Auto-format Python code by black

--- src/validate_yaml.py
+++ src/validate_yaml.py
@@ -2,17 +2,23 @@ import os
 import yaml
 import sys
 
+
 def validate_yaml(file_path, count, total_files, failed_flag):
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             yaml.safe_load(file)
         print(f"YAML syntax is valid in {file_path} ({count}/{total_files})")
     except yaml.YAMLError as e:
         print(f"Error in {file_path} ({count}/{total_files}): {e}")
         failed_flag[0] = True
 
+
 def check_yaml_files(folder_path):
-    yaml_files = [filename for filename in os.listdir(folder_path) if filename.endswith((".yaml", ".yml"))]
+    yaml_files = [
+        filename
+        for filename in os.listdir(folder_path)
+        if filename.endswith((".yaml", ".yml"))
+    ]
     total_files = len(yaml_files)
     failed_flag = [False]
 
@@ -22,6 +28,7 @@ def check_yaml_files(folder_path):
 
     if failed_flag[0]:
         sys.exit(1)
+
 
 if __name__ == "__main__":
     folder_to_check = "command"
