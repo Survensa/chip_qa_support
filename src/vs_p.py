7922db674fe5051d834c879fbc1c93aa9440a93f
Suraj Seenivasan <suraj@graniteriverlabs.in>
2023-12-05 00:27:05 +0530

    Update vs_p.py

--- src/vs_p.py
+++ src/vs_p.py
@@ -102,7 +102,7 @@ def extract_testcase_details_from_html(test_plan_type, header_tag, second_header
 	html_path = main_html_path
 	
     with open(html_path) as file:
-    soup = BeautifulSoup(file, "lxml")
+    	soup = BeautifulSoup(file, "lxml")
 
     header_tags = soup.find_all("h1", {"id": True})
     first_header_tag = [tag for tag in header_tags if tag.text == header_tag][0]
